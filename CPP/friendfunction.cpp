c1524de3e29b147781e42e4a5c60f9260a87271d
Scarred healer <devansh.shrivastava55@gmail.com>
2021-03-31 10:23:12 +0530

    yay

--- CPP/friendfunction.cpp
+++ CPP/friendfunction.cpp
@@ -2,16 +2,15 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-friend int friendly_func(int, int,int );
 class test
 {
 private:
   int a,b,c;
 public:
   void getdata()
+  {
     cout<<"ENter value for three variables : ";
     cin>>a>>b>>c;
-    {
     cout<<endl;
   }
   void putdata()
@@ -19,9 +18,17 @@ public:
     cout<<"The numbers entered are : "<<endl;
     cout<<"A = "<<a<<endl<<"B = "<<b<<endl<<"C = "<<c<<endl;
   }
-  friend int friendly_func(int a,int b,int c)
-  {
-    return a+b+c;
-  }
+  friend int friendly_func(test t);
 
 };
+int friendly_func(test t)
+{
+  return int(t.a+t.b+t.c);
+}
+int main()
+{
+  test t1;
+  t1.getdata();
+  t1.putdata();
+  cout<<"Sum of the numbers is : "<<friendly_func(t1);
+}
