e8af003f5e76b8e7943f49d4aa1c4b0e9d41d56e
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-01 11:33:54 +0530

    Create RBR.cpp

---
+++ ash_cpp/RBR.cpp
@@ -0,0 +1,22 @@
+//Return by reference
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int & max(int &x,int &y);
+int main()
+{
+  int a,b,c;
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+  max(a,b)=425;
+  cout << "a=" << a << endl << "b="<<b;
+  return 0;
+}
+
+int & max(int &x, int &y)
+{
+  if (x>y)
+  return x;
+  else
+  return y;
+}
