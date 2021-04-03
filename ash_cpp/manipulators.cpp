5ec23ea4e30551bc047d9ae6bb4e7d8bd9fb5111
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-03 11:14:03 +0530

    this is dope ya know

---
+++ ash_cpp/manipulators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  int m,n,p;
+  m=2597;
+  n=10;
+  p=564;
+  cout << setw (10) << "First " << set(10) << m << endl
+  << setw(10) << "Second= " << setw(10) << n << endl
+  << setw(10) << "Third= " << setw(10) << p << endl;
+}
