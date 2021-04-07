80e3554d0057d4814b224137e0b1dc76a84533e1
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-07 13:44:38 +0530

    ni

--- ash_cpp/binary_overloading.cpp
+++ ash_cpp/binary_overloading.cpp
@@ -9,4 +9,30 @@ public:
     x=a;
     y=b;
   }
+  Add()
+  {
+    x=0;
+    y=0;
+  }
+  void show()
+  {
+    cout<<x<<"+i"<<y<<endl;
+  }
+  Add operator+(Add ob)
+  {
+    Add temp;
+    temp.x=x+ob.x;
+    temp.y=y+ob.y;
+    return temp;
+  }
+};
+
+int main()
+{
+  Add ob1(2,3),ob2(3,4),ob3;
+  ob3=ob1+ob2;
+  ob1.show();
+  ob2.show();
+  ob3.show();
+  return 0;
 }
