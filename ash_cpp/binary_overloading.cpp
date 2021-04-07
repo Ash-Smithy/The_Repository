4dffad8853f81e5a421667a40be2aa627939e125
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-07 13:37:42 +0530

    na

---
+++ ash_cpp/binary_overloading.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Add
+{
+  int x,y,z;
+public:
+  Add(int a,int b)
+  {
+    x=a;
+    y=b;
+  }
+}
--- ash_cpp/unary_overloading.cpp
+++ ash_cpp/unary_overloading.cpp
@@ -5,33 +5,29 @@ class UnaryOp
 {
   int x,y,z;
 public:
-
-  UnaryOp()
-  {
-    x=0;
-    y=0;
-    z=0;
-  }
   UnaryOp(int a,int b, int c)
   {
     x=a;
     y=b;
     z=c;
   }
+  void show()
+  {
+    cout<<x<<"\t"<<y<<"\t"<<z;
+    cout<<endl;
+  }
+  void operator-()
+  {
+    x=-x;
+    y=-y;
+    z=-z;
+  }
 };
-void UnaryOp :: operator- ()
-{
-  x=-x;
-  y=-y;
-  z=-z;
-}
 int main()
 {
   UnaryOp un(10,-40,70);
-  cout << "\n\nNumbers are:::\n";
-  un.display();
+  un.show();
   -un;
-  cout <<"\n\nNumbers are after overloaded minus (-) operator :::\n";
-  undisplay();
+  un.show();
   return 0;
 }
