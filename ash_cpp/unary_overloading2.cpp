25112fa55331d2add88adb066da20586696b8f74
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-09 13:16:15 +0530

    .

---
+++ ash_cpp/unary_overloading2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class complex
+{
+  int a,b,c;
+public:
+  complex()()
+  void getvalue()
+  {
+    cout<<"Enter the two Numbers:";
+    cin>>a>>b;
+  }
+}
+int main()
+{
+  complex obj;
+  obj.getvalue();
+  obj++;
+
+}
