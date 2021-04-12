f8b2eb486b6d0ebd7b8c7175a4c427da8dd8f3f0
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-12 14:13:51 +0530

    cpp ;-;

---
+++ ash_cpp/template_class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const size=3;
+template <class T>
+class vectors
+{
+  T* v;
+public:
+  vector()
+  {
+    v=new T[size];
+    for(int i=0;i<size;i++)
+    v[i]=0;
+  }
+  vector(T* a)
+  {
+    for(int i=0;i<size;i++)
+    sum+=this->v[i]*y.v[i];
+    return sum;
+  }
+};
+int main()
+{
+  int x[3]={1,2,3};
+  int y[3]={4,5,6};
+  vectors<int> v1;
+  vectors<int> v2;
+  v1=x;
+  v2=y;
+  int r=v1*v2;
+  cout<<"R="<<r;
+  return 0;
+}
