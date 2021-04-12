23b52eeb25b1fdceb297934839c816152bc04306
Scarred healer <devansh.shrivastava55@gmail.com>
2021-04-12 14:36:36 +0530

    baaaaaaaah

--- DEV_CPP/inclasstemplates.cpp
+++ DEV_CPP/inclasstemplates.cpp
@@ -1,17 +1,17 @@
-//Operator overloading using template class
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-
 const int size=3;
 template <class T>
-class vectors
+
+class vector
 {
+
   T* v;
 public:
   vector()
   {
-    v=new T[size];
+    v = new T[size];
     for(int i=0;i<size;i++)
     v[i]=0;
   }
@@ -22,21 +22,21 @@ public:
   }
   T operator*(vector &y)
   {
-    T sum=0;
-    for(int i=0;i<size;i++)
+    T sum = 0;
+    for(int i = 0;i<size;i++)
     sum+=this->v[i]*y.v[i];
     return sum;
   }
 };
 int main()
 {
-  int x[3]={1,2,3};
-  int y[3]={4,5,6};
+  int x[3]=(1,2,3);
+  int y[3]=(4,5,6);
   vectors<int> v1;
   vectors<int> v2;
   v1=x;
   v2=y;
-  int r=v1*v2;
-  cout<<"R="<<r;
+  int r = v1*v2;
+  cout<<"R = "<<r;
   return 0;
 }
