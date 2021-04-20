b214766b3850fbd7ba00304ec96df0a903eeb410
Scarred healer <devansh.shrivastava55@gmail.com>
2021-04-20 11:25:08 +0530

    ummm

--- DEV_CPP/inheritance_continue.cpp
+++ DEV_CPP/inheritance_continue.cpp
@@ -33,7 +33,57 @@ public:
     cout<<"Sum is : "<<sum;
   }
 };
+int not_main()
+{
+  C obj;
+  obj.getn1();
+  obj.getn2();
+  obj.add();
+  return 0;
+}
+class shape
+{
+protected:
+  float d1,d2;
+public:
+  void getd()
+  {
+    cout<<"Enter the value for d1 and d2 :: "<<endl;
+    cin>>d1>>d2;
+  }
+};
+class Rectangle: public shape
+{
+  float area;
+public:
+  void arearect()
+  {
+    area = d1*d2;
+    cout<<"Area of rectangle is :: "<<area<<endl;
+  }
+};
+class Triangle: public shape
+{
+  float area;
+public:
+  void areatri()
+  {
+    area = 0.5*d1*d2;
+    cout<<"Area of Triangle is :: "<<area<<endl;
+  }
+};
+int not_main2()
+{
+  Rectangle r;
+  Triangle t;
+  r.getd();
+  r.arearect();
+  t.getd();
+  t.areatri();
+  return 0;
+}
 int main()
 {
-  
+  not_main2();
+  return 0;
 }
