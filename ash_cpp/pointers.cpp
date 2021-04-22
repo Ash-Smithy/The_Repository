cc4f2dd6f2557797c97859ed7e69d937da6367d7
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-04-22 14:59:01 +0530

    asasdadsdfsdfew

---
+++ ash_cpp/pointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class item;
+{
+  float price;
+  int code;
+public:
+  void getdata()
+  {
+  cout << "Enter values for code and price";
+  cin >> code >> price;
+  }
+  void show()
+  {
+  cout <<"Code: " << code <<endl;
+  cout << "Price: " << price;
+  }
+};
+
+
+int main()
+{
+
+  item x,*ptr; //item *ptr=new item;
+  ptr=&x;
+  ptr->getdata(); //(*ptr).getdata()
+  pte->show(); //(*ptr).show()
+  return 0;
+}
