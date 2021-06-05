3b360c0b0f0f79cfc66ca262ed2d7178b5dd6257
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-06-05 11:48:37 +0530

    idk this ain't executin

---
+++ ash_cpp/sparse_matrix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  int a[4][4],sp[5][3];
+  int i,j,k,t,p,n;
+  cout <<"enter order of a matrix: ";
+  cin>>p>>n;
+  cout<<"Enter " << p*n << " Elements";
+  for(i=0;i<p;i++)
+  {
+    for(j=0;j<n;j++)
+    cin>>a[i][j];
+  }
+  k=1;
+  t=0;
+  for(i=0;i<p;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(a[i][j]!=0)
+      {
+        sp[k][0]=i;
+        sp[k][1]=j;
+        sp[k][2]=a[i][j];
+        k++;
+        t++
+      }
+    }
+  }
+  sp[0][0]=p;
+  sp[0][1]=n;
+  sp[0][2]=t;
+  cout<<"\n sparse matrix is:\n";
+  for(i=0;i<=t;i++)
+   {
+     for(j=0;j<3;j++)
+     {
+       cout<<sp[i][j]<< " ";
+     }
+     cout<<"\n"
+   }
+}
