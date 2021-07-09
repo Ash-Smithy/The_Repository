1bb3ca41acbbd0ffa0683b50eb9a142e966d5379
Scarred healer <devansh.shrivastava55@gmail.com>
2021-07-09 17:43:13 +0530

    uiuiuiui

---
+++ DEV_CPP/linear_seach.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+  int arr[10]={5,3,7,23,8,2,9,4,2,1};
+  int i,x;
+  for(i=0;i<10;i++)
+  {
+    cout<<setw(3)<<arr[i]<<setw(3);
+  }
+  cout<<"\nEnter element to search :: ";
+  cin>>x;
+  for(i = 0 ;i< 10; i++)
+  {
+    if(arr[i] == x)
+    {
+      cout<<"element "<<x<<" found at position :: "<<i+1<<endl;
+      break;
+    }
+    else
+    {
+      continue;
+    }
+  }
+  return 0;
+}
