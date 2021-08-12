677dcbfd8b0998bf85f2ace4ff5b443845048805
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-12 14:42:51 +0530

    Update test.py
    fgh

--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,4 +1,4 @@
 import math
 num = input("enter a number to find factorial of : ")
 fact = math.factorial(int(num))
-print("The factorial of the number is : ",fact)
+print("The factorial of the number is : ", fact)
