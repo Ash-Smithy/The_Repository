f915f7c2142e5248d96bba8f200f15ef3968e9fb
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-13 13:14:30 +0530

    for loop

--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,4 +1,4 @@
-import math
-num = input("enter a number to find factorial of : ")
-fact = math.factorial(int(num))
-print("The factorial of the number is : ", fact)
+my_list = [1,2,3,4,5,6,7,8,9,10]
+i = 0
+for i in my_list:
+    print(i)
