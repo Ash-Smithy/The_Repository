2b7eb09ae0ab7aea991e6fa65824c5cf2d87cb88
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-14 12:11:17 +0530

    ppii

--- Python_Programs/binary_search.py
+++ Python_Programs/binary_search.py
@@ -1,4 +1,4 @@
-list_to_search = [8,5,7,6,9]
+list_to_search = [5,6,8,9,10]
 n=5
 flag = 0
 for i in list_to_search:
@@ -7,18 +7,11 @@ for i in list_to_search:
 x = int(input("\nEnter the value to search - "))
 
 print("\nFirst sorting the list/array \n")
-temp = 0
-for i in range(n-1):
-    for j in range(n-i):
-        if(list_to_search[j]>list_to_search[j+1]):
-            temp = list_to_search[j]
-            list_to_search[j]=list_to_search[j+1]
-            list_to_search[j+1]=temp
-print("\nlist after sorting is \n")
+
 print(list_to_search,"\n")
 lb = 0
 ub = n
-mb = 0
+
 while(flag!=1):
     mb = lb+((ub-lb)/2)
     if(ub<lb):
@@ -30,4 +23,4 @@ while(flag!=1):
         ub = mb-1
     elif(list_to_search[mb]==x):
         print("\nThe element is found at position ",mb+1)
-        flag = 1
+        flag = 1    
