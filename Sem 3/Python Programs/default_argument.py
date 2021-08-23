67dd331603389656de6120e43133adb4cfa28681
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-23 11:05:01 +0530

    default in main

---
+++ Sem 3/Python Programs/default_argument.py
@@ -0,0 +1,6 @@
+#default arguments
+def display(name, course = "BTech"):
+    print("name : " + name)
+    print("Course : ", course)
+display(course = "BCA",name = "Arav")
+display(name = "Reyansh")
