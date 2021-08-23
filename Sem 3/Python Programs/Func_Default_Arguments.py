f7cbadedf8e7f967961c84d29198261756b59050
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-08-23 11:14:07 +0530

    la

---
+++ Sem 3/Python Programs/Func_Default_Arguments.py
@@ -0,0 +1,5 @@
+def display(name, course = "Btech"):
+    print("Name: " + name)
+    print("Course: ", course)
+display(course = "BCA", name = "Arav")
+display(name = "Reyansh")
---
+++ Sem 3/Python Programs/Func_Lamda-Anonymus.py
@@ -0,0 +1,2 @@
+sum=lambda x,y:x+y
+print('sum is',sum(3,5))
