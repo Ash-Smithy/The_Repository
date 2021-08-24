6155a54f2749d345f98e326598d716f20896991a
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-24 10:10:54 +0530

    my changes bruv

---
+++ DEv_python_stuff/modules_n_namespace.py
@@ -0,0 +1,6 @@
+# module1
+def repeat_x(x):
+    return x*2
+# module2
+def repeat_x(x):
+    return x**2
---
+++ DEv_python_stuff/tochecknamespace.py
@@ -0,0 +1,2 @@
+import modules_n_namespace
+print("Result is : ",modules_n_namespace.repeat_x(10))
