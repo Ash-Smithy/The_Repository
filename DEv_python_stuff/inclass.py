291e02fe265d073406e8b3d731f3c29f8a22f61d
Scarred healer <devansh.shrivastava55@gmail.com>
2021-08-28 10:08:46 +0530

    commit

--- DEv_python_stuff/inclass.py
+++ DEv_python_stuff/inclass.py
@@ -1,21 +1,8 @@
-#all about strings
-def string_example1():
-    message = "Hello"
-    index = 0
-    for i in message:
-        print("Message(",index,")",i)
-        index = index + 1
-def String_ex2():
-    str1 = "Hello"
-    str2 = "World"
-    str3 = str1+str2
-    print(str3)
-def String_ex3():
-    str1 = "Hello"
-    str2 = "World"
-    str2+=str1
-    print(str2)
-string_example1()
-String_ex2()
-String_ex3()
+#more string stuff
 
+str1 = "jello"
+str2 = "nani?!"
+print(id(str1))
+print(id(str2))
+str1 +=str2
+print(id(str1))
