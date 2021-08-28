040cd4c69062058ee56c84bb21a615e812446a80
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-08-28 10:40:13 +0530

    kommit

---
+++ Sem3/PythonPrograms/Immutable_Strings.py
@@ -0,0 +1,6 @@
+str1="hello"
+str2="world"
+id(str1)
+id(str2)
+str1+=str2
+print(str1)
---
+++ Sem3/PythonPrograms/String-built-in-Functions.py
@@ -0,0 +1,8 @@
+str="hello"
+print(str.center(10,'*'))
+print(str.capitalize())
+
+
+message="helloworldhelloworld"
+str1="he"
+print(message.count(str,0,len(message)))
