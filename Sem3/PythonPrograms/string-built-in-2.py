2f1aee1706626513e09ae9ad173a4af74fb7537b
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-08-30 11:29:17 +0530

    no

--- Sem3/PythonPrograms/string-built-in-2.py
+++ Sem3/PythonPrograms/string-built-in-2.py
@@ -2,9 +2,20 @@ str3="Is this your bag?"
 print(str3.rfind("is"))
 print(str3.rindex("your",0,len(str3)))
 
-message="Jamesbond007"
+message="JamesBond007"
+print("name")
 print(message.isalnum())
 print(message.isalpha())
 print(message.isdigit())
-print(message.islower())
-print(message.isspace())
+message3="ABC"
+print(message3.islower())
+print(message3.isspace())
+print(message3.isupper())
+message2="pyton is Oops Language"
+print(message2.swapcase())
+print(message2.title())
+
+
+str1="haleo"
+str2="World"
+print(str2.join(str1))
