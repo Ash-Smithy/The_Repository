bbea74adf3296cfb140380c287f19688e1654a01
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-08-30 10:54:07 +0530

    hmpf

--- Sem3/PythonPrograms/String-built-in-Functions.py
+++ Sem3/PythonPrograms/String-built-in-Functions.py
@@ -1,8 +1,16 @@
-str="hello"
+str0="hello"
 print(str.center(10,'*'))
 print(str.capitalize())
 
 
-message="helloworldhelloworld"
+message1="helloworldhelloworld"
 str1="he"
-print(message.count(str,0,len(message)))
+print(message.count(str1,0,len(message)))
+
+message2="She is"
+str2="he"
+print(message.count(str2,0,len(message)))
+
+
+str3="Is this your bag?"
+print(str.rfind("is"))
---
+++ Sem3/PythonPrograms/string-built-in-2.py
@@ -0,0 +1,10 @@
+str3="Is this your bag?"
+print(str3.rfind("is"))
+print(str3.rindex("your",0))
+
+message="Jamesbond007"
+print(message.isalnum())
+print(message.isalpha())
+print(message.isdigit())
+print(message.islower())
+print(message.isspace())
