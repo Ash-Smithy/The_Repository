5d00628e8ae5251b275c563eaf044217170e3797
Scarred healer <devansh.shrivastava55@gmail.com>
2021-09-01 09:22:16 +0530

    yes

---
+++ DEv_python_stuff/String_slicing.py
@@ -0,0 +1,6 @@
+#python program to demonstrate string slicing
+str1="Python"
+print("The given string is : ",str1)
+print("slicing the string from 1st letter to 4th : ",str1[0:4])
+print("The reverse of the string is : ",str1[::-1])
+print("Printing only the second letters from the start : ",str1[::2])
