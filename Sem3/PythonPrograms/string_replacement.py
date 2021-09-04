6041e0ea06d8adbffe0ddf4087965cc1709357be
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-09-04 12:32:56 +0530

    Update string_replacement.py

--- Sem3/PythonPrograms/string_replacement.py
+++ Sem3/PythonPrograms/string_replacement.py
@@ -5,6 +5,13 @@ rep='ocean'
 new_string=re.sub(patt,rep,ahem)
 print(new_string)
 
-pattern="[a-zA-Z]"
+pattern=r"[a-zA-Z]+ \d"
 matches = re.findall(pattern,"VDI 20104, Maruthi Suzuki Cars in India")
 print(matches)
+
+pattern2=r"[a-zA-Z}+ \d+"
+matchas=re.finditer(pattern2,"LXI 2013, VXI 2015,VDI 20104, Maruthi Suzuki Cars in India")
+for match in matchas:
+    print("Match fount at: ", match.start())
+    print("Match fount at: ", match.end())
+    print("Match fount at: ", match.span())
