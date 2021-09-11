2e0ccc96577168d55d2906c38125111d7f736057
Scarred healer <devansh.shrivastava55@gmail.com>
2021-09-11 17:04:30 +0530

    mb

---
+++ DEv_python_stuff/in_college_assigned_stuff/list.py
@@ -0,0 +1,2 @@
+while(flag != 0):
+a = int(input("enter choice \n1.) for input elements in list \n2.) for printing the list \n3.) for appending elements to the list \n4.) delete the elements from the list \n :::"))
