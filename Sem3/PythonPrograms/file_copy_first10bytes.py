be1e7b72bb6d9ed03b2a0853d00541658afe443a
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-10-20 11:13:03 +0530

    no

--- Sem3/PythonPrograms/file_copy_first10bytes.py
+++ Sem3/PythonPrograms/file_copy_first10bytes.py
@@ -1,6 +1,6 @@
 #Program that copies first 10 byes of a file into another
 with open("file.txt","r") as file:
-    with open("file2.txt","r") as file2:
+    with open("file2.txt","w") as file2:
         buf=file.read(10)
         file2.write(buf)
 print("File copied")
