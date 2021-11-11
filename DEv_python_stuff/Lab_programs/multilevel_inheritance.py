8c61208fee05fcb6694e5536a2f2010025ccab04
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-11-11 10:59:22 +0530

    haha

--- DEv_python_stuff/Lab_programs/multilevel_inheritance.py
+++ DEv_python_stuff/Lab_programs/multilevel_inheritance.py
@@ -1,13 +1,13 @@
-class B1(object):
-    def __init__(self):
-        super(B1,self).__init__()
-        print("Base Class")
-class B2(object):
-    def __init__(self):
-        super(B2,self).__init__()
-        print("B2 class")
-class Derived(B1,B2):
-    def __init__(self):
-        super(Derived,self).__init__()
-        print("Derived class")
-D=Derived()
+class Person:
+    def name(self):
+        print("Name:")
+class Teacher(Person):
+    def Qualification(self):
+        print("PhD is must")
+class HoD(Teacher):
+    def experience(self):
+        print("Experience -> 15 years")
+hod = HoD()
+hod.name()
+hod.Qualification()
+hod.experience()
