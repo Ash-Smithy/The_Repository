0f00e5ab7bd4e826224581fb020b5d79917ad412
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2021-11-20 11:01:52 +0530

    no

--- Sem3/PythonPrograms/GUI Programming/Label.py
+++ Sem3/PythonPrograms/GUI Programming/Label.py
@@ -1,3 +1,7 @@
 from tkinter import *
 window=Tk()
-lbl=Label(window,text="Hello WOrld", fg='red', font=("Helvetica",16))
+lbl=Label(window,text="Hewwo Wowrld", fg='red', font=("Helvetica",16))
+lbl.place(x=75,y=105)
+window.title('Hello Python')
+window.geometry("300x300")
+window.mainloop()
