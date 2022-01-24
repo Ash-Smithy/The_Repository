8fef39b2631af84e38aae8b459e1e050794fd4ab
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2022-01-24 21:07:44 +0530

    sike

---
+++ Sem4/Java/multiplication.java
@@ -0,0 +1,10 @@
+public class multiplication{
+    public static void main(String args[]) {
+        int i,x=5;
+        System.out.print("Multiplication table of "+x);
+        for(i=0;i<=12;i++)
+            {
+                System.out.printf("%d x %d = %d\n",x,i,x*i);
+            }
+    }
+}
