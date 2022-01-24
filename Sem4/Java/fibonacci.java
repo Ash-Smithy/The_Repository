338f199b0a0673fa7cc5e18429a85d97588fb4b7
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2022-01-24 21:32:59 +0530

    .

---
+++ Sem4/Java/fibonacci.java
@@ -0,0 +1,17 @@
+public class fibonacci {
+    public static void main(String args[])
+    {
+        int i=0,a=0,b=1,c=0,k=5;
+        System.out.println(a);
+        System.out.println(b);
+        while(i<k)
+        {
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+            i++;
+        }        
+
+    }
+}
--- Sem4/Java/multiplication.java
+++ Sem4/Java/multiplication.java
@@ -1,10 +1,11 @@
+//Multiplication Table
 public class multiplication{
     public static void main(String args[]) {
         int i,x=5;
-        System.out.print("Multiplication table of "+x);
+        System.out.println("Multiplication table of "+x);
         for(i=0;i<=12;i++)
             {
-                System.out.printf("%d x %d = %d\n",x,i,x*i);
+                System.out.println(""+x+" x "+i+" = "+x*i);
             }
     }
 }
