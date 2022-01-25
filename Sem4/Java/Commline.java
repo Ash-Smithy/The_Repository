a7436242271ee2ac96d31cba944ed5b1ccdda900
Scarred healer <80884490+Scarredhealer@users.noreply.github.com>
2022-01-25 13:33:33 +0530

    some weird changes

---
+++ Sem4/Java/Commline.java
@@ -0,0 +1,10 @@
+public class Commline {
+    public static void main(String args[])
+    {
+        int len = args.length;
+        for(int i = 0;i<len;i++)
+        {
+            System.out.println("First arguemnt : "+args[i]);
+        }
+    }
+}
---
+++ Sem4/Java/java_d3v/another_sample.java
@@ -0,0 +1,38 @@
+package java_d3v;
+
+class classname 
+{
+    int x=10,y=20;
+    void putdata()
+    {
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+    }
+}
+class Rectangle
+{
+    int length,width;
+    void getdata(int x,int y)
+    {
+        length = x;
+        width = y;
+    }
+    int rectArea()
+    {
+        int area = length*width;
+        return area;   
+    }
+}
+public class another_sample
+{
+    public static void main(String args[])
+    {
+        classname obj1 = new classname();//creating object of class and allocating memory 
+        obj1.putdata();//calls the method for obj1 object
+
+        Rectangle r1 = new Rectangle();
+        r1.getdata(10,5); //calls getdata and pases x,y values
+        int area = r1.rectArea(); //calls rectArea and returns area
+        System.out.println("Area of rectangle is : "+area);
+    }
+}
