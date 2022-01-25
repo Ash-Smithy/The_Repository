f180b9fa25fc2d541baca093cbf4df712e595bc7
AshSmith001 <80884527+AshSmith001@users.noreply.github.com>
2022-01-25 13:33:59 +0530

    komit

---
+++ Sem4/ash_java/rectangle.java
@@ -0,0 +1,22 @@
+class Rectangle {
+    int l,b;
+    void getdata(int x,int y)
+    {
+        l=x;
+        b=y;
+    }
+    int area()
+    {
+        int a=l*b;
+        return(a);
+    }
+public class rectangle
+{
+    public static void main(String args[])
+    {
+        Rectangle r1=new Rectangle();
+        r1.getdata(10,5);
+        int area=r1.area();
+        System.out.println("Area of rectangle is: "+area);
+    }
+}
